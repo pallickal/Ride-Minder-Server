9fa2a379d654c677e0dcbee5dc864c84399c3d54
Lindsay Pallickal <pa_no_llic_spam_kal@g-male.com>
2016-11-11 01:51:06 -0500

    refactor: Fix linting errors and use ES6 imports in model loader

--- src/sequelize/models/index.js
+++ src/sequelize/models/index.js
@@ -1,17 +1,20 @@
 'use strict';
 
-var fs        = require('fs');
-var path      = require('path');
-var Sequelize = require('sequelize');
-var basename  = path.basename(module.filename);
-var env       = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '/../config.js')[env];
-var db        = {};
+import fs from 'fs';
+import path from 'path';
+import Sequelize from 'sequelize';
+import cfg from '../config';
+
+var env = process.env.NODE_ENV || 'development';
+var config = cfg[env];
+var basename = path.basename(module.filename);
+var db = {};
+var sequelize;
 
 if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+  sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
 fs
@@ -20,7 +23,7 @@ fs
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
   .forEach(function (file) {
-    var model = sequelize['import'](path.join(__dirname, file));
+    var model = sequelize.import(path.join(__dirname, file));
     db[model.name] = model;
   });
 
